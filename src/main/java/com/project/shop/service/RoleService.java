517de115c709855972b31ed4f8cc0c53b0f214ba
Jacek <mrwp450@gmail.com>
2021-04-13 13:22:12 +0200

    added role service

---
+++ src/main/java/com/project/shop/service/RoleService.java
@@ -0,0 +1,16 @@
+package com.project.shop.service;
+
+import com.project.shop.model.dto.RoleDto;
+
+import java.util.List;
+
+public interface RoleService {
+
+    List<RoleDto> getAllRoles(int page, int size);
+
+    List<RoleDto> getRoleByName(String name);
+
+    RoleDto saveRole(RoleDto role);
+
+    RoleDto getRoleById(int id);
+}
