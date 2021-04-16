33d263685d537e04973325694616d5163b12a09d
Jacek <mrwp450@gmail.com>
2021-04-16 15:11:03 +0200

    changed springfox do springdoc

--- src/main/java/com/project/shop/config/SwaggerConfiguration.java
+++ src/main/java/com/project/shop/config/SwaggerConfiguration.java
@@ -1,23 +1,49 @@
 package com.project.shop.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi.builder()
+                .group("Api")
+                .pathsToMatch("/api/**")
                 .build();
     }
+
+    @Bean
+    public OpenAPI apiInfo() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(
+                        new Info()
+                                .title("Shop project Rest Api")
+                                .description("Rest Api for shop project web app")
+                );
+
+    }
+
 
 }
